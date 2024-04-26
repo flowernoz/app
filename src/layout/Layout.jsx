528689fe6a894d0ff1f89e8331fe004a42d9ed3e
Flowernoza <cadillac2111@Gulnozas-Mac.local>
2024-04-26 21:25:00 +0500

    learn

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,11 +2,10 @@ import React, { memo } from "react";
 import "./Layout.css";
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import Header from "../components/header/Header";
-
+import { FaArrowLeft } from "react-icons/fa";
 const Layout = () => {
   let navigate = useNavigate();
-  const location = useLocation().pathname;
-  console.log(location);
+  const location = useLocation();
   const goBack = () => {
     navigate(-1);
   };
@@ -15,7 +14,11 @@ const Layout = () => {
       <Header />
       <main className="main_layout">
         <div className="container">
-          {location.pathname !== "/" && <button onClick={goBack}>Back</button>}
+          {location.pathname !== "/" && (
+            <button className="back" onClick={goBack}>
+              <FaArrowLeft />{" "}
+            </button>
+          )}
           <div className="main_container">{<Outlet />}</div>
         </div>
       </main>
