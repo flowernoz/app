f2df471ae187f4a3ceaa4a5fcae1335da39fbd66
Avazbek <ssamuray225@gmail.com>
2024-06-01 20:51:58 +0500

    new

--- src/pages/appInfo/AppInfo.jsx
+++ src/pages/appInfo/AppInfo.jsx
@@ -4,9 +4,9 @@ function AppInfo() {
     return (
         <div className='container'>
             <div className="app_info">
-                <p className='owner_info'>Teacher: Bekzod Murodullaev</p>
+                <p className='owner_info'>Teacher: Murodillayev Behzod</p>
                 <p className='owner_contact'>Contact: +998 93 949 97 14</p>
-                <p className='owner_telegram'>Telegram: <a href="https://t.me/MBBhd"><i>
+                <p className='owner_telegram'>Telegram: <a href="https://t.me/Murodillayev_Behzod"><i>
                     Bekzod Murodullaev</i></a></p>
                 <p className='owner_info'>Dasturchi: Avazbek Toshpulatov</p>
                 <p className='owner_contact'>Contact: +998 99 226 23 23</p>
